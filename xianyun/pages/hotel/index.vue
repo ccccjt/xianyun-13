9754cf82c94d47c6c5012c6e95e2ab4b1f160579
cgj <584041060@qq.com>
2019-08-10 11:45:25 +0800

    酒店首页头部组件静态添加

--- xianyun/pages/hotel/index.vue
+++ xianyun/pages/hotel/index.vue
@@ -1,39 +1,49 @@
 <template>
-    <div class="w">
-<!-- 酒店页面地区查询组件 -->
-    <div>
+  <div class="w">
+    <!-- 酒店页面地区查询组件 -->
+    <div class="title">
+      <el-breadcrumb separator-class="el-icon-arrow-right">
+        <el-breadcrumb-item :to="{ path: '/hotel' }">酒店</el-breadcrumb-item>
+        <el-breadcrumb-item>南京市酒店预订</el-breadcrumb-item>
+      </el-breadcrumb>
+      <HotelHead />
 
-
-        </div>
-      
-<!-- 酒店筛选组件 -->
-        <div class="screen"> 
-            <Screen/>
-            
-        </div>
-        
     </div>
+
+    <!-- 酒店筛选组件 -->
+    <div class="screen">
+      <Screen />
+
+    </div>
+
+  </div>
 </template>
 
 <script>
+import HotelHead from "@/components/hotel/hotelHead2"
 import Screen from '@/components/hotel/screen'
 export default {
   components: {
-      Screen
+    HotelHead,
+    Screen
   }
 }
 </script>
 
 <style>
-    #container {width:500px; height: 500px; }  
-.screen{
-    margin-top: 50px;
-    height: 80px;
+.screen {
+  margin-top: 50px;
+  height: 80px;
 }
-.w{
-        width: 1000px;
-        margin: 0 auto;
-        height: 1000px;
+.w {
+  width: 1000px;
+  margin: 0 auto;
+  height: 1000px;
+}
+.title {
+  width: 1000px;
+  margin: 0 auto;
+  padding-top: 20px;
 }
 </style>
 
