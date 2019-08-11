36c24b7dd4f3ee42973585b8d3f16c12a2758df7
田振毅 <ttzy0202@gmail.com>
2019-08-11 16:29:47 +0800

    攻略

--- xianyun/pages/post/index.vue
+++ xianyun/pages/post/index.vue
@@ -1,216 +1,304 @@
 <template>
-  <div class="post">
-    <!-- 左侧热门城市 -->
-    <div class="post-left"></div>
-    <!-- 右侧布局 -->
-    <div class="post-wrapper">
-      <!-- input输入框 -->
-      <el-input placeholder="请输入想去的地方，比如：'广州'"></el-input>
-      <i class="el-icon-search"></i>
-      <!-- 推荐 北上广 -->
-      <div class="search-recommend">
-        <i>推荐：</i>
-        <span>广州</span>
-        <span>上海</span>
-        <span>北京</span>
-      </div>
-      <!-- 推荐攻略 -->
-      <div class="post-title">
-        <h4>推荐攻略</h4>
-        <el-button type="primary" round>
-          <i class="el-icon-edit"></i> 写游记
-        </el-button>
-      </div>
-      <!-- 渲染内容 -->
-      <el-divider></el-divider>
-      <div class="post-content">
-        <!-- 左侧图片 -->
-        <el-card shadow="hover">
-          <el-image
-            style="width:100%"
-            src="http://p1-q.mafengwo.net/s11/M00/B4/92/wKgBEFt6ZqaAJeK7AAbj58wpNlY06.jpeg?imageView2%2F2%2Fw%2F1360%2Fq%2F90"
-          ></el-image>
-        </el-card>
-        <!-- 右侧内容 -->
-        <div class="post-content-right">
-          <p>小蛮腰广州塔，怎么玩才不遗憾？</p>
-          <p>造型独特的“小蛮腰”广州塔，犹如一颗璀璨的明珠屹立在珠江边，成为羊城新八景之一。别看她“身材苗条”，塔内可是蕴藏了丰富的休闲玩乐，等待您去掀开她的面纱。这座直逼云霄的高塔，如何在有限的时间打卡不同楼层？</p>
-          <!-- 底层 -->
-          <el-row>
-            <span class="el-item">
-              <i class="el-icon-location-information"></i>
-              <span>广州市</span>
-            </span>
-            <!-- 头像 -->
-            <span class="el-item">
-              by
-              <i class="el-icon-s-custom"></i>
-              <span style="color:orange">玩转地球者</span>
-            </span>
-            <span class="el-item">
-              <i class="el-icon-view"></i>
-              <span>188</span>
-            </span>
-            <h5>
-              <span>3</span>赞
-            </h5>
+  <div class="container">
+    <el-row type="flex" justify="space-between">
+      <!-- 左侧菜单栏 -->
+
+      <!-- 右侧内容 -->
+      <div class="post-wrapper">
+        <div class="search-wrapper">
+          <el-row type="flex" justify="space-between" align="middle" class="search-bar">
+            <input
+              type="text"
+              placeholder="请输入想去的地方，比如：'广州'"
+              v-model="city"
+              @keyup.enter="handleSearch"
+            />
+            <el-button type="primary" icon="el-icon-search">搜索</el-button>
           </el-row>
+          <div class="search-recommend">
+            推荐：
+            <span v-for="(item, index) in ['奥尔良 ','拉萨 ',' 西双版纳']" :key="index">{{item}}</span>
+          </div>
         </div>
+        <el-row type="flex" justify="space-between" align="middle" class="post-title">
+          <h4>推荐攻略</h4>
+          <el-button type="primary" size="small" icon="el-icon-edit">写游记</el-button>
+        </el-row>
+
+        <div class="post-list">
+          <postRight v-for="(item, index) in posts" :key="index" :data="item"></postRight>
+        </div>
+
+        <el-row type="flex" justify="center" style="margin-top:10px;">
+          <el-pagination
+            @size-change="handleSizeChange"
+            @current-change="handleCurrentChange"
+            :current-page="Math.floor(start / limit) + 1"
+            :page-sizes="[4, 8, 10, 15]"
+            :page-size="limit"
+            layout="total, sizes, prev, pager, next, jumper"
+            :total="total"
+          ></el-pagination>
+        </el-row>
       </div>
-      <!-- 分页 -->
-      <div class="block">
-        <el-pagination
-          @size-change="handleSizeChange"
-          @current-change="handleCurrentChange"
-          :current-page="currentPage4"
-          :page-sizes="[3, 5, 8, 10]"
-          :page-size="100"
-          layout="total, sizes, prev, pager, next, jumper"
-          :total="40"
-        ></el-pagination>
-      </div>
-    </div>
+    </el-row>
   </div>
 </template>
 
 <script>
+import postRight from "@/components/post/postRight";
+
 export default {
   data() {
     return {
-      currentPage4: 4
+      posts: [],
+      cities: [],
+
+      city: this.$route.query.city || "",
+      start: 0,
+      limit: 4,
+      total: 0
     };
   },
-  methods: {
-    handleSizeChange(val) {
-      console.log(`每页 ${val} 条`);
-    },
-    handleCurrentChange(val) {
-      console.log(`当前页: ${val}`);
+  components: {
+    postRight
+  },
+  watch: {
+    $route(value) {
+      const { city } = value.query;
+      this.city = city;
+      this.start = 0;
+      this.getPost();
     }
+  },
+  methods: {
+    getPost() {
+      const { api } = this.$store.state;
+      const params = {
+        _start: this.start,
+        _limit: this.limit
+      };
+
+      if (this.city) {
+        params.city = this.city;
+      }
+
+      this.$axios({
+        url: "http://157.122.54.189:9095/posts",
+        params
+      }).then(res => {
+        const { data, total } = res.data;
+        this.posts = data;
+        this.total = total;
+      });
+    },
+
+    handleSizeChange(value) {
+      this.limit = value;
+      this.start = 0;
+      this.getPost();
+    },
+
+    handleCurrentChange(value) {
+      this.start = this.limit * (value - 1);
+      this.getPost();
+    },
+
+    // 搜索
+    handleSearch(value) {
+      if (typeof value == "string") {
+        this.city = value;
+      }
+
+      this.start = 0;
+      this.getPost();
+    },
+
+    // 获取城市
+    getCities() {
+      const { api } = this.$store.state;
+
+      this.$axios({
+        url: "http:127.0.0.1:1337/posts/cities"
+      }).then(res => {
+        const { data } = res.data;
+        this.cities = data;
+      });
+    }
+  },
+
+  mounted() {
+    this.getPost();
+    this.getCities();
   }
 };
 </script>
 
-<style lang="less" scoped>
-.post {
-  display: flex;
+<style scoped lang="less">
+.container {
   width: 1000px;
-  margin: 30px auto;
-  .post-left {
-    width: 260px;
-    height: 200px;
-    background: red;
-  }
-  //右侧布局
-  .block {
-    margin-top: 30px;
-  }
-  .post-wrapper {
-    position: relative;
-    width: 700px;
-    margin-left: 30px;
-    // 搜素图标
-    /deep/ .el-icon-search {
-      position: absolute;
-      top: 8px;
-      left: 650px;
-      font-size: 30px;
-      color: orange;
-      text-indent: 10px;
-    }
-    /deep/ .el-input {
-      box-sizing: border-box;
-      border: 3px solid orange;
-      .el-input__inner {
-        border: 0;
-      }
-    }
-    .search-recommend {
-      margin-top: 10px;
-      margin-bottom: 20px;
-      > span,
-      i {
-        font-size: 12px;
-        color: #ccc;
-      }
-      > span:hover {
-        text-decoration: underline;
-        color: orange;
-        cursor: pointer;
-      }
-    }
-    // 推荐攻略
-    .post-title {
-      display: flex;
-      justify-content: space-between;
-      > h4 {
-        font-size: 18px;
-        font-weight: 400;
-        color: orange;
-        position: relative;
-      }
-      > h4::after {
-        display: block;
-        content: "";
-        width: 72px;
-        height: 3px;
-        background: orange;
-        position: absolute;
-        bottom: 0px;
-        left: 0px;
-      }
-    }
-    // 主要内容
-    .el-divider {
-      margin: 0;
-    }
-    .post-content {
-      display: flex;
-      margin-top: 20px;
-      /deep/ .el-card {
-        box-sizing: border-box;
-        width: 220px;
-        height: 150px;
-        .el-card__body {
-          padding: 0;
-        }
-      }
-      .post-content-right {
-        width: 470px;
-        margin-left: 10px;
+  margin: 0 auto;
+  padding: 20px 0;
+}
 
-        > p:nth-child(1) {
-          margin-top: 5px;
-          margin-bottom: 10px;
-          font-size: 18px;
-          cursor: pointer;
+/* 菜单栏 */
+.menus-wrapper {
+  position: relative;
+  width: 260px;
+  z-index: 2;
+
+  .menus {
+    width: 260px;
+    border: 1px #ddd solid;
+    border-right: none;
+    border-bottom: none;
+    box-shadow: 0 0 1px #f5f5f5;
+    z-index: 2;
+  }
+
+  .menu-item {
+    height: 40px;
+    line-height: 40px;
+    border-bottom: 1px #ddd solid;
+    border-right: 1px #ddd solid;
+    padding: 0 20px;
+    font-size: 14px;
+    position: relative;
+    z-index: 2;
+
+    &:after {
+      display: block;
+      content: "";
+      width: 10px;
+      height: 10px;
+      border-right: 1px #999 solid;
+      border-top: 1px #999 solid;
+      transform: rotate(45deg);
+      position: absolute;
+      right: 20px;
+      top: 15px;
+    }
+
+    &.active {
+      border-right-color: #fff;
+      color: orange;
+
+      &:after {
+        border-right-color: orange;
+        border-top-color: orange;
+      }
+    }
+  }
+
+  .sub-menus {
+    position: absolute;
+    left: 260px;
+    top: 0;
+    width: 350px;
+    padding: 10px 20px;
+    box-sizing: border-box;
+    background: #fff;
+    border: 1px #ddd solid;
+
+    ul li {
+      font-size: 14px;
+      line-height: 1.5;
+
+      * {
+        vertical-align: middle;
+      }
+
+      i {
+        color: orange;
+        font-size: 24px;
+        font-style: italic;
+      }
+
+      strong {
+        margin: 0 10px;
+        color: orange;
+        font-weight: normal;
+        &:hover {
+          text-decoration: underline;
         }
-        p:nth-child(1):hover {
-          color: orange;
-        }
-        p:nth-child(2) {
-          cursor: pointer;
-          height: 63px;
-          margin-bottom: 15px;
-          overflow: hidden;
-          color: rgba(0, 0, 0, 0.705);
-        }
-        /deep/.el-row {
-          display: flex;
-          font-size: 12px;
-          color: #999;
-          .el-item {
-            margin-right: 15px;
-          }
-          h5 {
-            color: orange;
-            font-size: 14px;
-            margin-left: 200px;
-          }
+      }
+
+      span {
+        color: #999;
+        &:hover {
+          text-decoration: underline;
         }
       }
     }
   }
 }
-</style>
 
+.post-wrapper {
+  width: 700px;
+}
+
+.search-wrapper {
+  .search-bar {
+    width: 100%;
+    box-sizing: border-box;
+    height: 40px;
+    line-height: 40px;
+    border: 3px orange solid;
+    border-right: 0;
+
+    input {
+      flex: 1;
+      padding: 0 20px;
+      line-height: 40px;
+      outline: none;
+      border: none;
+      background: none;
+    }
+
+    i {
+      font-size: 24px;
+      color: orange;
+      font-weight: bold;
+      margin-right: 10px;
+    }
+  }
+
+  .search-recommend {
+    padding: 10px 0;
+    font-size: 12px;
+
+    span {
+      margin-right: 5px;
+      &:hover {
+        color: orange;
+        text-decoration: overline;
+        cursor: pointer;
+      }
+    }
+  }
+}
+
+/* 标题 */
+.post-title {
+  padding-bottom: 10px;
+  border-bottom: 1px #eee solid;
+  position: relative;
+
+  h4 {
+    font-weight: normal;
+    font-size: 18px;
+    color: orange;
+
+    &:after {
+      display: block;
+      content: "";
+      width: 72px;
+      height: 2px;
+      background: orange;
+      position: absolute;
+      bottom: 0;
+      left: 0;
+    }
+  }
+}
+</style>
